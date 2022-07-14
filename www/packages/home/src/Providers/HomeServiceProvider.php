ca1dbd698342c948cc1496eac83cd272877cb7cc
Hoang Tu <tuvo@pandavietnamsoftware.com>
2022-07-14 17:48:34 +0700

    Basic Init Project

---
+++ www/packages/home/src/Providers/HomeServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Packages\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HomeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
